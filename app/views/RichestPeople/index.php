72d1710df93fbf71280dbae267461c0399a36977
boderoode <bo_deroode@outlook.com>
2022-10-18 10:07:29 +0200

    Rows are now deletable from the table

--- app/views/RichestPeople/index.php
+++ app/views/RichestPeople/index.php
@@ -1,6 +1,6 @@
 <ul>
     <li>
-        <a href="<?= URLROOT; ?>/landingpages/index">Ga terug naar de hoofdpagina!</a>
+        <a href="<?= URLROOT; ?>/landingpages/index">Ga terug naar de hoofdpagina</a>
     </li>
 </ul>
 
