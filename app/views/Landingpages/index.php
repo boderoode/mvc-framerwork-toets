07fa25b46cbc70510552457548eee22ac248b788
boderoode <bo_deroode@outlook.com>
2022-10-18 10:08:37 +0200

    site is fully functioning

--- app/views/Landingpages/index.php
+++ app/views/Landingpages/index.php
@@ -2,6 +2,6 @@
 
 <ul>
     <li>
-    <h3><a href="<?= URLROOT; ?>/RichestPeoples/index">show richest people!</a></h3>
+    <h3><a href="<?= URLROOT; ?>/RichestPeoples/index">show richest people.</a></h3>
     </li>
 </ul>
